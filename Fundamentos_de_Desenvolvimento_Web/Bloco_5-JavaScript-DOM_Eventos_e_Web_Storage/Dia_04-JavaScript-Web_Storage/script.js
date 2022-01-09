4ec0e3536d61db9f8f3bfb04c2fe0f292ec5bda8
lucas_almeida <lucas.almdiniz@gmail.com>
2022-01-09 20:24:21 -0300

    Adicionadas funcoes para salvar as mudancas da pagina no locaStorage

--- Fundamentos_de_Desenvolvimento_Web/Bloco_5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_04-JavaScript-Web_Storage/script.js
+++ Fundamentos_de_Desenvolvimento_Web/Bloco_5-JavaScript-DOM_Eventos_e_Web_Storage/Dia_04-JavaScript-Web_Storage/script.js
@@ -1,3 +1,4 @@
+// Cria lista de cores
 function showColors(classOfList) {
     let arrayOfColors = ['white', 'black', 'green', 'blue', 'red', 'yellow', 'orange'];
     let ulColor = document.createElement('ul');
@@ -13,9 +14,11 @@ function showColors(classOfList) {
     return ulColor;
 }
 
+// Opções de modificações de cores
 const body = document.getElementsByTagName('body');
-const divBackgroundColorScreen = document.querySelector ('#divBackgroundColorScreen');
+const divBackgroundColorScreen = document.querySelector('#divBackgroundColorScreen');
 const buttonBackgroundColorScreen = document.querySelector('#buttonBackgroundColorScreen');
+
 function bodyStyle(color) {
     for (let index = 0; index < body.length; index += 1) {
         body[index].style.backgroundColor = color;
@@ -25,6 +28,7 @@ function bodyStyle(color) {
 const backgroundText = document.getElementsByClassName('backgroundText');
 const divBackgroundColorText = document.querySelector('#divBackgroundColorText');
 const buttonBackgroundColorText = document.querySelector('#buttonBackgroundColorText');
+
 function backgroundTextStyle(color) {
     for (let index = 0; index < backgroundText.length; index += 1) {
         backgroundText[index].style.backgroundColor = color;
@@ -34,24 +38,25 @@ function backgroundTextStyle(color) {
 const text = document.getElementsByClassName('text');
 const divTextColor = document.querySelector('#divTextColor');
 const buttonTextColor = document.querySelector('#buttonTextColor');
-function TextStyle(color) {
+
+function textStyle(color) {
     for (let index = 0; index < text.length; index += 1) {
         text[index].style.color = color;
     }
 }
 
-
+// Escolhe o local para modicação de cor
 function selectThePlace(color, place) {
     if (place === divBackgroundColorScreen) {
         bodyStyle(color);
     } else if (place === divBackgroundColorText) {
         backgroundTextStyle(color);
     } else if (place === divTextColor) {
-        TextStyle(color);
+        textStyle(color);
     }
 }
-        
 
+// Modifica a cor do elemento com opções de cor
 function changeColor(placeToSearchListOfColor, buttonToTouch) {
     function searchListOfColors() {
         let searchUlColor = document.querySelector('.listColorSelected');
@@ -62,7 +67,7 @@ function changeColor(placeToSearchListOfColor, buttonToTouch) {
             event.currentTarget.remove();
         }
 
-        if (searchUlColor !== null){
+        if (searchUlColor !== null) {
             placeToSearchListOfColor.removeChild(searchUlColor);
         } else {
             ulColor = showColors('listColorSelected');
@@ -70,7 +75,7 @@ function changeColor(placeToSearchListOfColor, buttonToTouch) {
             ulColor.addEventListener('click', updateColor);
         }
 
-        
+
     }
 
     buttonToTouch.addEventListener('click', searchListOfColors);
@@ -80,6 +85,7 @@ changeColor(divBackgroundColorScreen, buttonBackgroundColorScreen);
 changeColor(divBackgroundColorText, buttonBackgroundColorText);
 changeColor(divTextColor, buttonTextColor);
 
+// Modifica o tamanho da fonte do texto
 const buttonFontSize = document.querySelector('#buttonFontSize');
 const divFontSize = document.querySelector('#divFontSize');
 
@@ -90,9 +96,10 @@ function setSizeOnParagraphs(size) {
     }
 }
 
+const sizeList = ['15px', '17px', '19px', '21px', '23px'];
+
 function addEventAtUlSize(ulSize) {
     function selectSize(event) {
-        const sizeList = ['15px', '17px', '19px', '21px', '23px'];
         const elementSizeSelected = event.target.innerText.split(' ')[1];
         const size = sizeList[elementSizeSelected - 1];
         setSizeOnParagraphs(size);
@@ -110,7 +117,7 @@ function showFontSize() {
         const size = document.createElement('li');
         size.className = 'textFontSize';
         size.innerText = 'Tamanho ' + (index + 1);
-        ulSize.appendChild(size); 
+        ulSize.appendChild(size);
     }
     divFontSize.appendChild(ulSize)
     addEventAtUlSize(document.getElementsByClassName('textFontSize'));
@@ -130,8 +137,9 @@ function changeFontSize() {
 
 changeFontSize();
 
+// Modifica o espaçamento entre linhas do texto
 const buttonLineHeight = document.querySelector('#buttonLineHeight');
-const divLineHeight  = document.querySelector('#divLineHeight');
+const divLineHeight = document.querySelector('#divLineHeight');
 
 function setLineHeightOnParagraphs(lineHeight) {
     const paragraphs = document.getElementsByClassName('paragraph');
@@ -140,9 +148,10 @@ function setLineHeightOnParagraphs(lineHeight) {
     }
 }
 
+const lineHeightList = ['23px', '25px', '27px', '29px', '31px'];
+
 function addEventAtUlLineHeight(ulLineHeight) {
     function selectLineHeight(event) {
-        const lineHeightList = ['23px', '25px', '27px', '29px', '31px'];
         const elementLineHeightSelected = event.target.innerText.split(' ')[3];
         const lineHeight = lineHeightList[elementLineHeightSelected - 1];
         setLineHeightOnParagraphs(lineHeight);
@@ -160,13 +169,13 @@ function showLineHeight() {
         const liLineHeight = document.createElement('li');
         liLineHeight.className = 'lineHeight';
         liLineHeight.innerText = 'Espaçamento entre linhas ' + (index + 1);
-        ulLineHeight.appendChild(liLineHeight); 
+        ulLineHeight.appendChild(liLineHeight);
     }
     divLineHeight.appendChild(ulLineHeight)
     addEventAtUlLineHeight(document.getElementsByClassName('lineHeight'));
 }
 
-function changeLineHeight () {
+function changeLineHeight() {
     function checkLineHeight() {
         const ulLineHeight = document.querySelector('#ulLineHeight');
         if (ulLineHeight === null) {
@@ -180,8 +189,9 @@ function changeLineHeight () {
 
 changeLineHeight();
 
+// Modifica o tipo de fonte do texto
 const buttonFontFamily = document.querySelector('#buttonFontFamily');
-const divFontFamily  = document.querySelector('#divFontFamily');
+const divFontFamily = document.querySelector('#divFontFamily');
 
 function setFontFamilyOnParagraphs(fontFamily) {
     const paragraphs = document.getElementsByClassName('paragraph');
@@ -201,22 +211,23 @@ function addEventAtUlFontFamily(ulFontFamily) {
     }
 }
 
+const fontFamilyList = ['Acme', 'Fuzzy Bubbles', 'Indie Flower', 'Nosifer', 'sans-serif'];
+
 function showFontFamily() {
     const ulFontFamily = document.createElement('ul');
     ulFontFamily.id = 'ulFontFamily';
-    const fontFamilyList = ['Acme', 'Fuzzy Bubbles', 'Indie Flower', 'Nosifer', 'sans-serif'];
     for (let index = 0; index < 5; index += 1) {
         const liFontFamily = document.createElement('li');
         liFontFamily.className = 'fontFamily';
         liFontFamily.innerText = fontFamilyList[index];
         liFontFamily.style.fontFamily = fontFamilyList[index]
-        ulFontFamily.appendChild(liFontFamily); 
+        ulFontFamily.appendChild(liFontFamily);
     }
     divFontFamily.appendChild(ulFontFamily)
     addEventAtUlFontFamily(document.getElementsByClassName('fontFamily'));
 }
 
-function changeFontFamily () {
+function changeFontFamily() {
     function checkFontFamily() {
         const ulFontFamily = document.querySelector('#ulFontFamily');
         if (ulFontFamily === null) {
@@ -229,3 +240,132 @@ function changeFontFamily () {
 }
 
 changeFontFamily();
+
+// Armazena dados no localStorage
+let datasOfChanges = {
+    'screenColor': undefined,
+    'textBgtColor': undefined,
+    'textColor': undefined,
+    'fontSize': undefined,
+    'lineHeight': undefined,
+    'fontFamily': undefined
+};
+
+function saveChanges(key, value) {
+    let savedDatas = JSON.parse(localStorage.getItem('pageChanges'));
+    if (savedDatas === null) {
+        savedDatas = {};
+    }
+    savedDatas[key] = value;
+    localStorage.setItem('pageChanges', JSON.stringify(savedDatas));
+}
+
+function getValueBody(event) {
+    const ulBgColorScreen = event.target.nextElementSibling;
+    if (ulBgColorScreen !== null) {
+        function addEventAtulBackgroundColorScreen(event) {
+            const colorOfScreen = event.target.innerText;
+            saveChanges('screenColor', colorOfScreen);
+        }
+        ulBgColorScreen.addEventListener('click', addEventAtulBackgroundColorScreen);
+    }
+}
+buttonBackgroundColorScreen.addEventListener('click', getValueBody);
+
+function getValueBgColorText(event) {
+    const ulBgColorText = event.target.nextElementSibling;
+    if (ulBgColorText !== null) {
+        function addEventAtulBgColorText(event) {
+            const colorOfTextBg = event.target.innerText;
+            saveChanges('textBgtColor', colorOfTextBg);
+        }
+        ulBgColorText.addEventListener('click', addEventAtulBgColorText);  
+    }
+}
+buttonBackgroundColorText.addEventListener('click', getValueBgColorText);
+
+function getValueTextColor(event) {
+    const ulcolorOfText = event.target.nextElementSibling;
+    if (ulcolorOfText !== null) {
+        function addEventAtulcolorOfText(event) {
+            const colorOfText = event.target.innerText;
+            saveChanges('textColor', colorOfText);
+        }
+        ulcolorOfText.addEventListener('click', addEventAtulcolorOfText);
+    } 
+}
+buttonTextColor.addEventListener('click', getValueTextColor);
+
+function getValueFontSize(event) {
+    const ulsizeOfFont = event.target.nextElementSibling;
+    if (ulsizeOfFont !== null) {
+        function addEventAtulsizeOfFont(event) {
+            let sizeOfFont = event.target.innerText;
+            sizeOfFont = sizeList[sizeOfFont.split(' ')[1] - 1];
+            saveChanges('fontSize', sizeOfFont);
+        }
+        ulsizeOfFont.addEventListener('click', addEventAtulsizeOfFont);
+    } 
+}
+buttonFontSize.addEventListener('click', getValueFontSize);
+
+function getValueLineHeight(event) {
+    const ulheightOfLine = event.target.nextElementSibling;
+    if (ulheightOfLine !== null) {
+        function addEventAtulheightOfLine(event) {
+            let heightOfLine = event.target.innerText;
+            heightOfLine = lineHeightList[heightOfLine.split(' ')[3] - 1];
+            saveChanges('lineHeight', heightOfLine);
+        }
+        ulheightOfLine.addEventListener('click', addEventAtulheightOfLine);
+    }
+}
+buttonLineHeight.addEventListener('click', getValueLineHeight);
+
+function getValueFontFamily(event) {
+    const ultypeOfFont = event.target.nextElementSibling;
+    if (ultypeOfFont !== null) {
+        function addEventAtultypeOfFont(event) {
+            const typeOfFont = event.target.innerText;
+            saveChanges('fontFamily', typeOfFont);
+        }
+        ultypeOfFont.addEventListener('click', addEventAtultypeOfFont);
+    }
+}
+buttonFontFamily.addEventListener('click', getValueFontFamily);
+
+// Carregar dados salvos no storage
+function loadSavedDatas() {
+    const datasOfLocalStorage = JSON.parse(localStorage.getItem('pageChanges'));
+    if(datasOfLocalStorage!== null) {
+        if (datasOfLocalStorage.screenColor !== undefined) {
+            bodyStyle(datasOfLocalStorage['screenColor']);
+        }
+    }
+    if(datasOfLocalStorage !== null) {
+        if (datasOfLocalStorage.textBgtColor !== undefined) {
+            backgroundTextStyle(datasOfLocalStorage['textBgtColor']);
+        }
+    }
+    if(datasOfLocalStorage !== null) {
+        if (datasOfLocalStorage.textColor !== undefined) {
+            textStyle(datasOfLocalStorage['textColor']);
+        }
+    }
+    if(datasOfLocalStorage !== null) {
+        if (datasOfLocalStorage.fontSize !== undefined) {
+            setSizeOnParagraphs(datasOfLocalStorage['fontSize']);
+        }
+    }
+    if(datasOfLocalStorage !== null) {
+        if (datasOfLocalStorage.lineHeight !== undefined) {
+            setLineHeightOnParagraphs(datasOfLocalStorage['lineHeight']);
+        }
+    }
+    if(datasOfLocalStorage !== null) {
+        if (datasOfLocalStorage.fontFamily !== undefined) {
+            setFontFamilyOnParagraphs(datasOfLocalStorage['fontFamily']);
+        }    
+    }
+};
+window.onload = loadSavedDatas();
