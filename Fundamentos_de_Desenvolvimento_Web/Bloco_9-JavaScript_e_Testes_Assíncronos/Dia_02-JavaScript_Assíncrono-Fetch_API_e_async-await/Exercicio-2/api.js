5644200cdf39dbaef31f708d683e9e6a1c5102ab
lucas_almeida <lucas.almdiniz@gmail.com>
2022-02-11 14:47:29 -0300

    Parte Bonus do exercicio 2 do dia 2 do bloco 9 realizado

--- Fundamentos_de_Desenvolvimento_Web/Bloco_9-JavaScript_e_Testes_Assíncronos/Dia_02-JavaScript_Assíncrono-Fetch_API_e_async-await/Exercicio-2/api.js
+++ Fundamentos_de_Desenvolvimento_Web/Bloco_9-JavaScript_e_Testes_Assíncronos/Dia_02-JavaScript_Assíncrono-Fetch_API_e_async-await/Exercicio-2/api.js
@@ -1,32 +1,58 @@
 const url = `https://api.coincap.io/v2/assets`;
 const arrayCoins = [];
-let coins;
-const tbody = document.getElementById('tbody');
+const tbodyUS = document.getElementById('tbody');
 
-function putCoins() {
-  coins.forEach(element => {
-    const newLine = document.createElement('tr');
-    const newCoinColumn = document.createElement('td');
-    newCoinColumn.innerText = element.name
-    const newSymbolColumn = document.createElement('td');
-    newSymbolColumn.innerText = element.symbol
-    const newValueColumn = document.createElement('td');
-    newValueColumn.innerText = parseFloat(element.priceUsd).toFixed(2);
-    newLine.appendChild(newCoinColumn);
-    newLine.appendChild(newSymbolColumn);
-    newLine.appendChild(newValueColumn);
-    tbody.appendChild(newLine);
-  });
-
+function createTd(valueTd) {
+  const td = document.createElement('td');
+  td.innerText = valueTd;
+  return td;
 }
 
-async function getCryptos() {
-  coins = await fetch(url)
+function createCurrencyPrice(price, symbol) {
+  if (!price) {
+    return '---';
+  }
+  let value = parseFloat(price, 10);
+    value = value.toLocaleString('pt-br', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  return `${symbol} ${value}`;
+}
+
+function putCoins(coinsUS, coinsBRL, place) {
+  coinsUS.forEach((element, index) => {
+    const newLine = document.createElement('tr');
+    const newCoinColumn = createTd(element.name);
+    const newSymbolColumn = createTd(element.symbol);
+    const newValueUSColumn = createTd(createCurrencyPrice(element.priceUsd, 'U$'));
+    const newValueBRLColumn = createTd(createCurrencyPrice((1 / coinsBRL[index]), 'R$'));
+    newLine.appendChild(newCoinColumn);
+    newLine.appendChild(newSymbolColumn);
+    newLine.appendChild(newValueUSColumn);
+    newLine.appendChild(newValueBRLColumn);
+    place.appendChild(newLine);
+  });
+}
+
+async function getCryptosBRL(coinsUS) {
+  const url_base = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/brl.json`;
+  try {
+    const response = await fetch(url_base);
+    const data = await response.json();
+    const selectedCoins = coinsUS.map((element) =>
+      data.brl[element['symbol'].toLowerCase()]);
+    return selectedCoins;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getCryptosUS() {
+  let coinsUS = await fetch(url)
   .then(response => response.json())
   .then(data => data.data)
   .catch(error => console.log(error));
-  coins = coins.slice(0, 50);
-  putCoins();
+  coinsUS = coinsUS.slice(0, 10);
+  const coinsBRL = await getCryptosBRL(coinsUS);
+  putCoins(coinsUS, coinsBRL, tbodyUS);
 }
 
-getCryptos();
+getCryptosUS();
